3bd64beb996f28da95e804b6b77d1a7c06e8bbce
Ghii15 <144661258+Ghii15@users.noreply.github.com>
2023-09-11 14:40:38 +0700

    Create Penjumlahan.cpp

---
+++ Penjumlahan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Masukkan Angka Pertama:";
+    cin>>a;
+    cout<<"masukkan Angka Kedua:";
+    cin>>b;
+    c=a+b;
+    cout<<"Hasilnya adalah:"<<a+b<<endl;
+    return 0;
+}
